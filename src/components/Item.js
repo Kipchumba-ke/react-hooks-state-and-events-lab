6ff57b179d4ea9708aff549d73a8e3346eec7189
Kelvin Kipchumba <chogekelvin90@gmail.com>
2024-12-01 16:48:53 +0300

    react hooks

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React , {useState} from "react";
 
 function Item({ name, category }) {
+  const [cart, setcart] =useState(false)
+
+  const toggleCart = () => {
+    setcart((prevcart) => !prevcart);
+  };
   return (
-    <li className="">
+    <li className={cart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCart}>{cart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
